59c92fd83882a5cd317fe1d874aad17f40c1bbc9
She-MO <nikitashp3@gmail.com>
2026-03-06 19:27:49 +0200

    Implement data extractor

---
+++ ETL/Extractor.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace ETL_project;
+
+public class Extractor
+{
+    private readonly string _filePath;
+
+    public Extractor(string filePath)
+    {
+        _filePath = filePath;
+    }
+    public IEnumerable<string[]> Extract()
+    {
+        using var reader = new StreamReader(_filePath);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        csv.Read();
+        csv.ReadHeader();
+
+        while (csv.Read())
+        {
+            yield return csv.Parser.Record;
+        }    
+    }
+}
